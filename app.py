dba2ab8c53520b8bf6f4c677cc3e83ccf920137d
Lee-Siyoung <tldud4861@gmail.com>
2023-04-25 20:39:52 +0900

    스타일 변화, 다중 언어 번역 및 녹음 기능 추가

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 from flask import Flask, render_template, request
+import openai_secret_manager
 import openai
 import os
 
 app = Flask(__name__)
 
+# OpenAI API 인증 정보 가져오기
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def translate(text):
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    data = request.get_json()
+    text = data['text']
+    target_language = data['target_language']
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=("번역: 영어를 한국어로 번역합니다. 만약 한국어이라면 영어로 번역합니다\n"
-                "입력: 문장\n"
-                "번역: 문장이 영어면 한국어로 번역하고 문장이 한국어면 영어로 번역한 문장입니다.  \n\n"
-                f"입력: {text}\n"
-                "번역: "),
+        prompt=(f"Translate the following text into {target_language}: {text}\n"
+                f"Translation:"),
         temperature=0.5,
         max_tokens=1024,
-        n=1,
+        n = 1,
         stop=None,
-        timeout=60,
+        timeout=30,
     )
+    translated_text = response.choices[0].text.strip()
+    return {'translated_text': translated_text}
 
-    translation = response.choices[0].text.strip()
-    return translation
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/translate", methods=["POST"])
-def translate_text():
-    text = request.form["text"]
-    translation = translate(text)
-    return render_template("index.html", translation=translation)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
